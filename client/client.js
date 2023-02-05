ef3b5d447480253b6dd10ac07a37b0fded97ac1f
viastolfi <vincent.astolfi@etu.uca.fr>
2023-02-05 15:01:42 +0100

    ajout de rooms pour le chat qui permettent de créer des sessions à uniquement deux joueurs

--- client/client.js
+++ client/client.js
@@ -1,16 +1,84 @@
-let button = document.getElementById('say');
+const player = {
+    host: false,
+    roomId: null,
+    username: "",
+    socketId: "",
+}
 
-let writeEvent = (text) => {
-    let parent = document.querySelector('#events');
-    
-    let el = document.createElement('li');
-    el.innerHTML = text;
+const sock = io();
 
-    parent.appendChild(el);
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const roomId = urlParams.get('room');
+
+if(roomId){
+    document.getElementById('start').innerText = 'Rejoindre';
+}
+
+const userNameInput = document.querySelector('#username');
+
+const userCard = document.getElementById('user-card');
+
+const linkToShare = document.getElementById('link-to-share');
+const shareCard = document.getElementById('share-card');
+
+const waitingArea = document.getElementById('waiting-area');
+
+const chat = document.getElementById('chat-form');
+
+// constante correspondante au élément permettant d'afficher la liste des rooms disponibles 
+const roomsCard = document.querySelector('#rooms-card');
+const roomsList = document.querySelector('#rooms-list');
+
+let friendUsername = "";
+
+sock.emit('get rooms');
+
+sock.on('list rooms', (rooms) => {
+    let html = "";
+
+    if(rooms.length > 0){
+        rooms.forEach(room => {
+            if(room.players.length !== 2){
+                html += `<li>
+                            <p>Salon de ${room.players[0].username} - ${room.id}</p>
+                            <button class="join-room" data-room="${room.id}">Rejoindre</button>
+                        </li>`;
+            }
+        });
+    }
+
+    if(html !== ""){
+        roomsCard.classList.remove('hidden-element');
+        roomsList.innerHTML = html;
+
+        for(const element of document.getElementsByClassName('join-room')){
+            element.addEventListener('click', joinRoom, false);
+        }
+    }
+});
+
+sock.on('join room', (roomId) => {
+    player.roomId = roomId;
+    linkToShare.innerHTML = `<a href="${window.location.href}?room=${player.roomId}" target="_blank">${window.location.href}?room=${player.roomId}</a>`;
+});
+
+sock.on('start chat', (players) => {
+    console.log(players);
+    startChat(players);
+});
+
+const writeEvent = function(text){
+    let parent = document.querySelector('#chat-box');
+
+    let newElement = document.createElement('li');
+    newElement.innerHTML = text;
+
+    parent.appendChild(newElement);
 };
 
-let onFormSummit = (e) => {
-    e.preventDefault();
+const onFormSubmit = function(event){
+    event.preventDefault();
 
     let input = document.querySelector('#chat');
     let text = input.value;
@@ -20,9 +88,50 @@ let onFormSummit = (e) => {
     sock.emit('message', text);
 };
 
-writeEvent('Welcome to RPS');
+const joinRoom = function() {
+    if(userNameInput.value !== ""){
+        player.username = userNameInput.value;
+        player.socketId = sock.id;
+        player.roomId = this.dataset.room;
 
-const sock = io();
+        sock.emit('playerData', player);
+
+        userCard.hidden = true;
+        waitingArea.classList.remove('hidden-element');
+        roomsCard.classList.add('hidden-element');
+    }
+}
+
+const onCreateRoom = function(event){
+    event.preventDefault();
+
+    player.username = userNameInput.value;
+
+    if(roomId){
+        player.roomId = roomId;
+    }else{
+        player.host = true;
+    }
+
+    player.socketId = sock.id;
+    userCard.hidden = true;
+    waitingArea.classList.remove('hidden-element');
+    roomsCard.classList.add('hidden-element');
+
+    sock.emit('playerData', player);
+}
+
+function startChat(players){
+    waitingArea.classList.add('hidden-element');
+    linkToShare.classList.add('hidden-element');
+    shareCard.classList.add('hidden-element');
+    chat.classList.remove('hidden-element');
+
+    const friend = players.find(p => p.socketId != player.socketId);
+    friendUsername = friend.username;
+}
+
 sock.on('message', writeEvent);
 
-document.querySelector('#chat-form').addEventListener('submit', onFormSummit);
+document.querySelector('#chat-form').addEventListener('submit', onFormSubmit);
+document.querySelector('#form').addEventListener('submit', onCreateRoom);
--- server/server.js
+++ server/server.js
@@ -1,30 +1,78 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
+const { Socket } = require('socket.io');
 
-const clientPath = `${__dirname}/../client`;
-console.log(`Serving static from ${clientPath}`);
+const express = require('express');
 
 const app = express();
-const server = http.createServer(app);
+const http = require('http').createServer(app);
+const clientPath = `${__dirname}/../client/`;
+const port = 8080;
 
-const io = socketio(server);
+const io = require('socket.io')(http);
+
+app.use(express.static(clientPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(clientPath);
+});
+
+http.listen(port, () => {
+    console.log(`Listening on htpp://localhost:${port}/`)
+});
+
+let rooms = [];
 
 io.on('connection', (sock) => {
-    console.log('Someone connected');
-    sock.emit('message', 'Hi, you are connected');
+
+    sock.on('playerData', (player) => {
+        console.log(`[connection] ${player.username}`);
+
+        let room = null;
+
+        if(!player.roomId){
+            room = createRoom(player);
+            console.log(`[create room] - ${room.id} - ${player.username}`);
+        }else{
+            room = rooms.find(r => r.id === player.roomId);
+
+            if(room === undefined){
+                return;
+            }
+
+            player.roomId = room.id;
+            room.players.push(player);
+        }
+
+        sock.join(room.id);
+
+        io.to(sock.id).emit('join room', room.id);
+
+        if(room.players.length === 2){
+            io.to(room.id).emit('start chat', room.players);
+        }
+    });
 
     sock.on('message', (text) => {
         io.emit('message', text);
     });
+
+    sock.on('get rooms', () => {
+        io.to(sock.id).emit('list rooms', rooms);
+    });
 });
 
-app.use(express.static(clientPath));
+function createRoom(player){
+    const room = {
+        id: roomId(),
+        players: []
+    };
 
-server.on('error', (err) => {
-    console.error('Server error :',err);
-});
+    player.roomId = room.id;
+    room.players.push(player);
+    rooms.push(room);
 
-server.listen(8080, () => {
-    console.log('RPS started on 8080');
-});
+    return room;
+}
+
+function roomId(){
+    return Math.random().toString(36).substr(2, 9);
+}
