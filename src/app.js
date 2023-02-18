04d9403fb29f7148d3b500e1caa6b39c2ee4034d
viastolfi <vincent.astolfi@etu.uca.fr>
2023-02-18 15:47:40 +0100

    test résolution du bug lié à la page web créer par le container

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const { Socket } = require('socket.io');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const http = require('http').createServer(app);
-const clientPath =  `${__dirname}/public/`;
 const port = 8080;
 
 let roomArray = []
@@ -13,7 +13,15 @@ http.listen(port, () => {
 })
 
 const io = require('socket.io')(http);
-app.use(express.static(clientPath));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/multiplayerHomePage', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/multiplayerHomePage.html'))
+});
 
 module.exports = {
     io,
